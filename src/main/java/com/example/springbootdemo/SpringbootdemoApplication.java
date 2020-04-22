e0789d64c5b34d1eb557dc0f356cdbfa1b4abde7
zgf <01117498@wisedu.com>
2020-04-22 15:42:50 +0800

    feat: 测试一个访问hello接口

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -2,8 +2,10 @@ package com.example.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
