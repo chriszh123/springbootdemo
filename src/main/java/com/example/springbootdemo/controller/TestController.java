b171abff4eb124fc44d75186fbdd311e692310a8
zgf <01117498@wisedu.com>
2020-04-23 14:02:28 +0800

    读取不同配置文件数据

--- src/main/java/com/example/springbootdemo/controller/TestController.java
+++ src/main/java/com/example/springbootdemo/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.example.springbootdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${biz.test.hello}")
+    private String controllerLbl;
+
     @GetMapping("/hello")
     public String hello() {
-        return "test";
+        return controllerLbl;
     }
 }
