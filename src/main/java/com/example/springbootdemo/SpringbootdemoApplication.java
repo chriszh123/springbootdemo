456847c8175c4298af6b99b686d6dc2dce1db7c4
zgf <01117498@wisedu.com>
2020-04-28 16:01:03 +0800

    vue后端 : web容器中进行部署Initializer

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -12,6 +12,6 @@ public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
+        System.out.println("启动成功，tks！");
     }
-
 }
